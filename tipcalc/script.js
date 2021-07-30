d93fbaca6035019f51bef7c22b45afde5d155fce
Sam Reilly <samreilly@gmail.com>
2021-07-30 22:24:26 +1000

    a very boring tip calculator

---
+++ tipcalc/script.js
@@ -0,0 +1,22 @@
+
+
+const calcTip = document.getElementById("calculate").addEventListener("click", calculateTip);
+
+function calculateTip() {
+
+    const billTotal = document.getElementById("billTotal").value;
+    const tipValue = document.getElementById("tipValue").value;
+    const people = document.getElementById("persons").value;
+    const tipTotal = document.getElementById("tipTotal");
+    const tipTotalPP = document.getElementById("tipTotalPP");
+
+    console.log(billTotal);
+    console.log(tipValue);
+
+    const totalTip = Math.round(billTotal * (tipValue / 100));
+    const totalPerPerson = Math.round(totalTip / people);
+    tipTotal.innerHTML = `$${totalTip}`;
+    tipTotalPP.innerHTML = `$${totalPerPerson}`;
+    console.log(totalTip);
+}
+
