658312701e9b2b7c13ce4440c66082cf87c5dfc7
Sam Reilly <samreilly@gmail.com>
2021-07-30 23:14:52 +1000

    tidying up

--- tipcalc/script.js
+++ tipcalc/script.js
@@ -1,24 +1,22 @@
-
-
-const calcTip = document.getElementById("calculate").addEventListener("click", calculateTip);
+const calcTip = document
+  .getElementById("calculate")
+  .addEventListener("click", calculateTip);
 
 function calculateTip() {
+  const billTotal = document.getElementById("billTotal").value;
+  const tipValue = document.getElementById("tipValue").value;
+  const people = document.getElementById("persons").value;
+  const tipTotal = document.getElementById("tipTotal");
+  const tipTotalPP = document.getElementById("tipTotalPP");
 
-    const billTotal = document.getElementById("billTotal").value;
-    const tipValue = document.getElementById("tipValue").value;
-    const people = document.getElementById("persons").value;
-    const tipTotal = document.getElementById("tipTotal");
-    const tipTotalPP = document.getElementById("tipTotalPP");
+  console.log(billTotal);
+  console.log(tipValue);
 
-    console.log(billTotal);
-    console.log(tipValue);
+  const totalTip = Math.round(billTotal * (tipValue / 100));
+  const totalPerPerson = Math.round(totalTip / people);
+  tipTotal.innerHTML = `$${totalTip}`;
+  tipTotalPP.innerHTML = `$${totalPerPerson}`;
+  console.log(totalTip);
 
-    const totalTip = Math.round(billTotal * (tipValue / 100));
-    const totalPerPerson = Math.round(totalTip / people);
-    tipTotal.innerHTML = `$${totalTip}`;
-    tipTotalPP.innerHTML = `$${totalPerPerson}`;
-    console.log(totalTip);
-
-    //TODO - needs to check inputs for 0 values
+  //TODO - needs to check inputs for 0 values
 }
-
